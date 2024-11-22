9b06e20ad83042935ceb88174bdd6043a90caa9a
pablosazycz <pablosazycz@gmail.com>
2024-11-21 23:03:20 -0300

    cambios en auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ function ensureAuthenticated(req, res, next) {
 }
 
 function isAdmin(req, res, next) {
-  if (req.isAuthenticated() && req.user.rol === 'admin') {
+  if (req.isAuthenticated() && req.user.rol === 'Admin') {
     return next();
   }
   res.status(403).send('Acceso denegado');
