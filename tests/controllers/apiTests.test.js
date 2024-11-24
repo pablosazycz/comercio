ff2ebbe5fce85905a563684d39a849645c0a95e6
mc780 <mconstantin0281@gmail.com>
2024-11-24 13:51:52 -0300

    se agregan mas casos
    se agregan mas casos

--- tests/controllers/apiTests.test.js
+++ tests/controllers/apiTests.test.js
@@ -19,7 +19,6 @@ let clienteAgent;
 
 // Antes de las pruebas
 beforeAll(async () => {
-
     if (mongoose.connection.readyState === 0) {
         mongoServer = await MongoMemoryServer.create();
         const uri = mongoServer.getUri();
@@ -111,7 +110,7 @@ afterEach(async () => {
 
 describe('POST /usuarios/registro', () => {
 
-    it('Debería registrar un nuevo usuario correctamente', async () => {
+    it('El Usuario se debería poder registrar correctamente', async () => {
         const nuevoUsuario = {
             nombre: 'Juan',
             apellido: 'Pérez',
@@ -135,7 +134,7 @@ describe('POST /usuarios/registro', () => {
         expect(usuarioEnDB.email).toBe(nuevoUsuario.email);
     });
 
-    it('Debería devolver un error si las contraseñas no coinciden', async () => {
+    it('El Usuario no debería poder registrarse si las contraseñas no coinciden', async () => {
         const usuario = {
             nombre: 'Pedro',
             apellido: 'González',
@@ -152,7 +151,7 @@ describe('POST /usuarios/registro', () => {
         expect(res.text).toContain('Las contraseñas no coinciden');
     });
 
-    it('Debería devolver un error si el email ya está registrado', async () => {
+    it('El Usuario no debería poder registrarse si el email ya está registrado', async () => {
         const usuarioDuplicado = {
             nombre: 'Carlos',
             apellido: 'Sánchez',
@@ -174,8 +173,8 @@ describe('POST /usuarios/registro', () => {
 
 // ===================== POST /API/LOGIN ==================
 
-describe('GET /categorias', () => {
-    it('Debería ingresar al sistema el Usuario que tiene rol de Admin', async () => {
+describe('POST /api/login', () => {
+    it('El Usuario que tiene rol de Admin debería poder ingresar al sistema', async () => {
         // Simula el login del usuario con rol de admin
         const res = await adminAgent
             .post('/api/login')
@@ -186,7 +185,7 @@ describe('GET /categorias', () => {
         expect(res.status).toBe(200);
     });
 
-    it('Debería ingresar al sistema el Usuario que tiene rol de Cliente', async () => {
+    it('El Usuario que tiene rol de Cliente debería poder ingresar al sistema', async () => {
         // Simula el login del usuario con rol de cliente
         const res = await clienteAgent
             .post('/api/login')
@@ -197,7 +196,7 @@ describe('GET /categorias', () => {
         expect(res.status).toBe(200);
     });
 
-    it('El usuario No debería ingresar al sistema con credenciales incorrectas', async () => {
+    it('El Usuario no debería ingresar al sistema con credenciales incorrectas', async () => {
         // Simula intento de login con credenciales incorrectas (c323hds)
         const res = await clienteAgent
             .post('/api/login')
@@ -217,8 +216,13 @@ describe('GET /categorias', () => {
 
 describe('GET /categorias', () => {
 
-    it("GET Categoria Un admin puede ver las categorias", async () => {
+    it("Un admin puede ver las categorias", async () => {
         const res = await adminAgent.get('/api/categorias');
+        expect(res.status).toBe(200); // Se cargan las categorias
+    });
+
+    it("Un cliente puede ver las categorias", async () => {
+        const res = await clienteAgent.get('/api/categorias');
         expect(res.status).toBe(200); // Se cargan las categorias
     });
 });
@@ -257,6 +261,36 @@ describe('POST /categorias', () => {
         expect(resPost.body).toHaveProperty('nombre', 'Categoría Test 2');
         expect(resPost.body).toHaveProperty('descripcion', 'Descripción de prueba 2');
     });
+
+    it("Un cliente no puede crear una categoría", async () => {
+        // Simula el login como cliente
+        const clienteLoginRes = await clienteAgent
+            .post('/api/login')
+            .send({
+                email: 'cliente@ejemplo.com',
+                password: 'cliente123',
+            });
+
+        // Accede a las cookies para seguir en la sesión iniciada
+        // Las separa antes del punto y coma y las une en una sola string
+        const cookies = clienteLoginRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ');
+
+        console.log("Cookies del cliente:", cookies);  // Imprime las cookies
+
+        // Definimos los datos para la nueva categoría
+        const newCategoria = {
+            nombre: 'Categoría Test 2',
+            descripcion: 'Descripción de prueba 2',
+        };
+
+        // Se hace la solicitud POST para crear la nueva categoría
+        const resPost = await clienteAgent
+            .post('/api/categorias')  // Ruta para crear la categoría
+            .set('Cookie', cookies)  // Fija las cookies para mantener la sesión
+            .send(newCategoria);  // Envía la nueva categoría
+
+        expect(resPost.status).toBe(403); //Acceso denegado
+    });
 });
 
 // ====================== PUT Categoria ===================
@@ -293,6 +327,36 @@ describe('PUT /categorias', () => {
         expect(resPost.body).toHaveProperty('nombre', 'Categoría Editada');
         expect(resPost.body).toHaveProperty('descripcion', 'Descripción editada');
     });
+
+    it("Un cliente no puede editar una categoría", async () => {
+        // Simula el login como cliente
+        const clienteLoginRes = await clienteAgent
+            .post('/api/login')
+            .send({
+                email: 'cliente@ejemplo.com',
+                password: 'cliente123',
+            });
+
+        // Accede a las cookies 
+        const cookies = clienteLoginRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ');
+
+        console.log("Cookies del cliente:", cookies);  // Imprime las cookies 
+
+        // Definimos datos para editar la categoría
+        const editarCategoria = {
+            nombre: 'Categoría Editada',
+            descripcion: 'Descripción editada',
+        };
+
+        // Hacemos la solicitud POST para editar la categoría
+        const resPost = await clienteAgent
+            .put(`/api/categorias/${categoria._id}`)  // Ruta para editar la categoría por su ID
+            .set('Cookie', cookies)  // Utiliza las cookies para mantener la sesión
+            .send(editarCategoria);  // Envía la categoría editada
+
+        // Verifica la respuesta
+        expect(resPost.status).toBe(403); //Acceso denegado
+    });
 });
 
 // ====================== DELETE Categoria ================
@@ -320,6 +384,30 @@ describe('DELETE /categorias', () => {
         // Verifica la respuesta
         expect(res.status).toBe(204);  // 204: Eliminación exitosa
     });
+
+    it("Un cliente no puede eliminar una categoría", async () => {
+        // Simula el login como admin
+        const clienteLoginRes = await adminAgent
+            .post('/api/login')
+            .send({
+                email: 'cliente@ejemplo.com',
+                password: 'cliente123',
+            });
+
+        // Guardamos las cookies para seguir en la sesión
+        const cookies = clienteLoginRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ');
+
+        console.log("Cookies del cliente:", cookies);  // Imprime las cookies 
+
+        // Hacemos la solicitud POST para eliminar la categoría
+        const res = await clienteAgent
+            .delete(`/api/categorias/${categoria._id}`)  // Ruta para eliminar la categoría por su ID
+            .set('Cookie', cookies)  // Utiliza las cookies 
+            .send();
+
+        // Verifica la respuesta
+        expect(res.status).toBe(403);  // Acceso denegado para el cliente
+    });
 });
 
 
@@ -329,35 +417,43 @@ describe('DELETE /categorias', () => {
 // ===================== GET /API/PRODUCTOS ==============
 
 describe('GET /productos', () => {
-    it('debería devolver todos los productos si el usuario tiene rol de admin', async () => {
+    it('Un admin debería poder ver todos los productos', async () => {
         const res = await adminAgent.get('/api/productos');
         expect(res.status).toBe(200);
         expect(res.body).toHaveLength(1); // Solo un producto en la BD
         expect(res.body[0].nombre).toBe('Producto Test');
     });
 
-    it('debería devolver producto por ID si el usuario tiene rol de admin', async () => {
+    it('Un admin debería poder ver un producto por ID', async () => {
         const res = await adminAgent.get(`/api/productos/${producto._id}`);
         expect(res.status).toBe(200);
         expect(res.body.nombre).toBe('Producto Test');
     });
 
-    it('debería devolver todos los productos si el usuario tiene rol de cliente', async () => {
+    it('Un cliente debería poder ver todos los productos', async () => {
         const res = await clienteAgent.get('/api/productos');
         expect(res.status).toBe(200);
         expect(res.body).toHaveLength(1);
         expect(res.body[0].nombre).toBe('Producto Test');
     });
 
-    it('debería devolver producto por ID si el usuario tiene rol de cliente', async () => {
+    it('Un cliente debería poder ver un producto por ID', async () => {
         const res = await clienteAgent.get(`/api/productos/${producto._id}`);
         expect(res.status).toBe(200);
         expect(res.body.nombre).toBe('Producto Test');
     });
 
-    it('debería devolver 404 si el producto no existe', async () => {
+    it('Un admin recibe 404 si el producto no existe', async () => {
         const invalidId = '60c72b2f9b1d8c9f75f6d4c3'; // ID inválido
         const res = await adminAgent.get(`/api/productos/${invalidId}`);
+        expect(res.status).toBe(404);
+        expect(res.body.error).toBe('Producto no encontrado');
+    });
+
+
+    it('Un cliente recibe 404 si el producto no existe', async () => {
+        const invalidId = '60c72b2f9b1d8c9f75f6d4c3'; // ID inválido
+        const res = await clienteAgent.get(`/api/productos/${invalidId}`);
         expect(res.status).toBe(404);
         expect(res.body.error).toBe('Producto no encontrado');
     });
@@ -400,6 +496,39 @@ describe('POST /productos', () => {
         expect(resPost.body).toHaveProperty('nombre', 'Nuevo Producto Test');
         expect(resPost.body).toHaveProperty('descripcion', 'Descripción del nuevo producto');
     });
+
+    it("Un cliente no puede crear un producto", async () => {
+        // Simula el login como admin
+        const clienteLoginRes = await clienteAgent
+            .post('/api/login')
+            .send({
+                email: 'cliente@ejemplo.com',
+                password: 'cliente123',
+            });
+
+        // Accede a las cookies para seguir en la sesión iniciada
+        // Las separa antes del punto y coma y las une en una sola string
+        const cookies = clienteLoginRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ');
+
+        console.log("Cookies del cliente:", cookies);  // Imprime las cookies
+
+        // Definimos los datos para el nuevo producto
+        const nuevoProducto = {
+            nombre: 'Nuevo Producto Test',
+            descripcion: 'Descripción del nuevo producto',
+            precio: 149.99,
+            categoria: categoria._id,
+        };
+
+        // Se hace la solicitud POST para crear un nuevo producto
+        const resPost = await clienteAgent
+            .post('/api/productos')  // Ruta para crear un producto
+            .set('Cookie', cookies)  // Usa las cookies para mantener la sesión
+            .send(nuevoProducto);  // Envía el nuevo producto
+
+        // Verifica la respuesta
+        expect(resPost.status).toBe(403); //Acceso denegado
+    });
 });
 
 // ====================== PUT Producto ===================
@@ -438,6 +567,38 @@ describe('PUT /productos', () => {
         expect(resPost.body).toHaveProperty('nombre', 'Producto editado');
         expect(resPost.body).toHaveProperty('descripcion', 'Descripción editada');
     });
+
+    it("Un cliente no puede editar una producto", async () => {
+        // Simula el login como admin
+        const clienteLoginRes = await clienteAgent
+            .post('/api/login')
+            .send({
+                email: 'cliente@ejemplo.com',
+                password: 'cliente123',
+            });
+
+        // Accede a las cookies 
+        const cookies = clienteLoginRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ');
+
+        console.log("Cookies del cliente:", cookies);  // Imprime las cookies 
+
+        // Definimos los datos para editar el producto
+        let productoAct = {
+            nombre: "Producto editado",
+            descripcion: "Descripción editada",
+            precio: 500,
+            categoria: categoria._id,
+        };
+
+        // Hacemos la solicitud POST para editar el producto
+        const resPost = await clienteAgent
+            .put(`/api/productos/${producto._id}`)  // Ruta para editar el producto por su ID
+            .set('Cookie', cookies)  // Utiliza las cookies seguir conectado
+            .send(productoAct);  // Envía los datos del producto editado
+
+        // Verifica la respuesta
+        expect(resPost.status).toBe(403);//Acceso denegado
+    });
 });
 
 
@@ -466,6 +627,29 @@ describe('DELETE /producto', () => {
             .send();
 
         expect(res.status).toBe(204);  // 204: Eliminación exitosa
+    });
+
+    it("Un cliente no puede eliminar un producto", async () => {
+        // Simula el login como cliente
+        const clienteLoginRes = await clienteAgent
+            .post('/api/login')
+            .send({
+                email: 'cliente@ejemplo.com',
+                password: 'cliente123',
+            });
+
+        // Guardamos las cookies para seguir en la sesión
+        const cookies = clienteLoginRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ');
+
+        console.log("Cookies del cliente:", cookies);  // Imprime las cookies
+
+        // Hacemos la solicitud POST para eliminar el producto
+        const res = await clienteAgent
+            .delete(`/api/productos/${producto._id}`)  // Ruta para eliminar el producto por ID
+            .set('Cookie', cookies)  // Utiliza las cookies 
+            .send();
+
+        expect(res.status).toBe(403);  // Acceso denegado
     });
 });
 
